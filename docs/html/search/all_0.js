7e6d849d33899b8f9d17a209d59e8865a9a636e0
Tony Josi <tonyjosinew@gmail.com>
2020-05-27 00:20:49 +0530

    added docs

---
+++ docs/html/search/all_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['_5fg_5fhash_5ftable_0',['_g_hash_table',['../struct__g__hash__table.html',1,'']]],
+  ['_5fght_5fitem_1',['_ght_item',['../struct__ght__item.html',1,'']]]
+];
---
+++ docs/html/search/files_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['legacy_5fmacros_2eh_23',['legacy_macros.h',['../legacy__macros_8h.html',1,'']]]
+];
